344637d20481508b8ba14460233479388bd03c06
Emily Tang <emjtang@stanford.edu>
2016-02-29 14:10:04 -0800

    adding dashboard.html for pink bagel

---
+++ mainController.js
@@ -0,0 +1,15 @@
+var cs142App = angular.module('cs142App', ['ngRoute']);
+
+cs142App.config(['$routeProvider',
+    function($routeProvider) {
+      $routeProvider.
+        when('/dashboard', {
+          templateUrl: 'dashboard.html',
+        }).
+        when('/assistant', {
+          templateUrl: 'index.html',
+        }).
+        otherwise({
+          redirectTo: '/'
+        });
+}]);
